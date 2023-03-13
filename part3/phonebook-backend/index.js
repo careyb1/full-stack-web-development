2262c8c263e997ab181605639fe78e1aa25db837
careyb1 <33967905+careyb1@users.noreply.github.com>
2023-03-13 12:01:04 +0200

    ex: 3.3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,10 +25,6 @@ let persons = [
   }
 ]
 
-//app.get('/', (request, response) => {
-//  response.send('<h1>Hello World!</h1>')
-//})
-
 app.get('/info', (request, response) => {
   const people = persons.length
   const time = new Date()
@@ -42,9 +38,20 @@ app.get('/info', (request, response) => {
         </p>
     </div>`)
 })
-  
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id ===id)
+
+  if (person){
+    response.json(person)
+  } else{
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
